b6bd5a84a5db2b866bd683968c42d1cc28644d52
Andre Roden <rodenan@hochschule-trier.de>
2017-07-18 14:37:03 +0200

    Tracklistenfüllfunktion und liste in html

--- Client/src/js/index.js
+++ Client/src/js/index.js
@@ -1,4 +1,5 @@
 var GoogleMapsLoader = require("google-maps");
+var JsonTracks;
 GoogleMapsLoader.KEY = "AIzaSyDphoZGe9mIqPoZ4e5aWG7RORkw-yH4JSA";
 GoogleMapsLoader.load(function (google) { 							//google-maps Karte laden
 	var map = new google.maps.Map(document.getElementById("map"),
@@ -18,7 +19,16 @@ function getTracks()
 	{
 		if(request.readyState === 4 && request.status ===200)
 		{
-			
+				JsonTracks=JSON.parse(request.responseText);			
 		}
-	}
-}
+	};
+	request.send();
+};
+
+
+function fillList()
+var  tracklist =document.getElementById("Trackliste");
+	for (var i=0;i<JsonTracks.length();i++)
+		var item= document.createElement(li)
+		item.appendChild(document.createTextNode(JsonTracks[i]))
+		tracklist.appendChild(item)
